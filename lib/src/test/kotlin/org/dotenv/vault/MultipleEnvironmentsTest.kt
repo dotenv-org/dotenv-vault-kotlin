f8028d1fb6de38201c30002d7f5390911e800088
marcel-rf <marcelr01@gmail.com>
2023-04-26 17:34:52 -0300

    add test for custom enviroment suffix.

--- lib/src/test/kotlin/org/dotenv/vault/MultipleEnvironmentsTest.kt
+++ lib/src/test/kotlin/org/dotenv/vault/MultipleEnvironmentsTest.kt
@@ -8,7 +8,7 @@ import kotlin.test.assertNull
 class MultipleEnvironmentsTest {
 
     @Test
-    fun loadVaultByDefault() {
+    fun loadVaultForProductionEnvironmentLoadsProdValuesForVariable() {
         val classUnderTest = dotenvVault() {
             directory = "multiEnvironments"
         }
@@ -18,5 +18,16 @@ class MultipleEnvironmentsTest {
         assertNotNull(prodVaultContent)
         val devVaultContent = classUnderTest.entries().find { it.key == "DOTENV_VAULT_DEVELOPMENT" }
         assertNotNull(devVaultContent)
+    }
+
+    @Test
+    fun loadVaultForCustomEnvironmentLoadsCorrectValuesForVariable() {
+        val classUnderTest = dotenvVault(key = "dotenv://:key_b2e7ae6abd29f1683ef59e2b3c7cd0d504889c904bc87b22b27fd18bfa95b959@dotenv.local/vault/.env.vault?environment=customEnv") {
+            directory = "multiEnvironments"
+        }
+        val decryptedValue = classUnderTest["MY_TEST_EV1"]
+        assertEquals("my custom ev 1", decryptedValue)
+        val prodVaultContent = classUnderTest.entries().find { it.key == "DOTENV_VAULT_CUSTOMENV" }
+        assertNotNull(prodVaultContent)
     }
 }
