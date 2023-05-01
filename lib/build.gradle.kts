1ef35876a81fc4152930a84cfd70dcd8457cc412
marcel-rf <marcelr01@gmail.com>
2023-05-01 10:59:53 -0300

    version bump

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -33,7 +33,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "org.dotenv.vault.kotlin"
             artifactId = "dotenv-vault-kotlin"
-            version = "0.0.1"
+            version = "0.0.2"
             from(components["java"])
         }
     }
